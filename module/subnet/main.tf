d58542eacbbf5d32e8497d4f6201e0b1f07ba481
suminhong <35161069+suminhong@users.noreply.github.com>
2021-03-26 16:22:43 +0900

    terraform test10 : if (count)

---
+++ module/subnet/main.tf
@@ -0,0 +1,30 @@
+resource "aws_subnet" "subnet" {
+  vpc_id                  = var.vpc_id
+  cidr_block              = var.public_subnet_cidr
+  availability_zone       = var.public_subnet_az
+  map_public_ip_on_launch = var.is_public
+
+  tags = {
+    Name = "${var.alltag}-subnet-${var.public_or_private[var.is_public]}"
+  }
+}
+
+resource "aws_route_table" "rtb" {
+  vpc_id = var.vpc_id
+
+  tags = {
+    Name = "${var.alltag}-rtb-${var.public_or_private[var.is_public]}"
+  }
+}
+
+resource "aws_route" "rtb" {
+  count                  = var.igw_only_public[var.is_public]
+  route_table_id         = aws_route_table.rtb.id
+  destination_cidr_block = "0.0.0.0/0"
+  gateway_id             = var.igw_id
+}
+
+resource "aws_route_table_association" "rtb" {
+  subnet_id      = aws_subnet.subnet.id
+  route_table_id = aws_route_table.rtb.id
+}
---
+++ stage/main.tf
@@ -0,0 +1,32 @@
+provider "aws" {
+  region = "ap-northeast-2"
+}
+
+module "vpc" {
+  source = "../module/vpc"
+
+  vpc_cidr = var.vpc_cidr
+  alltag   = var.alltag
+}
+
+module "public_subnet" {
+  source = "../module/subnet"
+
+  vpc_id             = module.vpc.vpc_id
+  public_subnet_cidr = var.public_subnet_cidr
+  public_subnet_az   = data.aws_availability_zones.available.names["${var.public_subnet_az}"]
+  is_public          = true
+  alltag             = var.alltag
+  igw_id             = module.vpc.igw_id
+}
+
+module "private_subnet" {
+  source = "../module/subnet"
+
+  vpc_id             = module.vpc.vpc_id
+  public_subnet_cidr = var.private_subnet_cidr
+  public_subnet_az   = data.aws_availability_zones.available.names["${var.private_subnet_az}"]
+  is_public          = false
+  alltag             = var.alltag
+  igw_id             = module.vpc.igw_id
+}
